2e6f14104692f8ac1894728ebe56e365cdf3f7e1
ThunderBoomer <CarrollCEC@gmail.com>
2013-11-03 16:20:48 -0500

    added error, private modifiers, and get methods

--- src/ga/Chrom.java
+++ src/ga/Chrom.java
@@ -3,17 +3,29 @@
  */
 package ga;
 
+//import java.util.*;
+
 /**
  * @author Corey
  *
  */
 public class Chrom implements GAparams{
 
-	int[] genes = new int[MAX_GENES];
+	private int[] genes = new int[MAX_GENES];
+	private int error;
 	
 	public Chrom(){
 	
 			//randomly populate genes array
 	
 	}
+	
+	public int getError(){
+		return error;
+	}
+	
+	public int[] getGenes(){
+		return genes;
+	}
+	
 }
